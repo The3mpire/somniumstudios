7db019c8b3ccf61db58f7d7f872ae6f12084e623
Karla Kraiss <karkraiss@gmail.com>
2017-02-12 16:36:33 -0700

    Puzzles are "done" (still need art)
    Puzzle system is working. Fade in/out in progress. Yes I know that the
    GameManager is broken shut up Boo

---
+++ SomniumStudios/Assets/_Scripts/Components/Puzzle/Draggable.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider2D))]
+
+public class Draggable : MonoBehaviour {
+    private Vector3 screenPoint;
+    //private Vector3 offset;
+    public bool isDraggable = true;
+    public GameObject receptacle;
+
+    //TODO put in puzzle manager
+    private int layerCount = 0;
+
+    void OnMouseDown() {
+        if (isDraggable) {
+            gameObject.GetComponent<SpriteRenderer>().sortingOrder = PuzzleManager.getLayerCounter();
+
+            screenPoint = Camera.main.WorldToScreenPoint(gameObject.transform.position);
+
+
+           // offset = gameObject.transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z));
+            Cursor.visible = false;
+        }
+    }
+
+    void OnMouseDrag() {
+        if (isDraggable) {
+            Vector3 curScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
+
+            Vector3 curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint);
+            transform.position = curPosition;
+        }
+    }
+
+    void OnMouseUp() {
+        if (receptacle.GetComponent<BoxCollider2D>().bounds.Contains(Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z)))) {
+            Snap();
+        }
+        Cursor.visible = true;
+    }
+
+    void Snap() {
+        Transform parentLocation = receptacle.transform;
+
+        receptacle.GetComponent<SpriteRenderer>().color = Color.gray;
+
+        transform.position = parentLocation.position;
+
+        GetComponent<SpriteRenderer>().sortingOrder = -1;
+
+        isDraggable = false;
+
+        GetComponent<BoxCollider2D>().enabled = false;
+        receptacle.GetComponent<BoxCollider2D>().enabled = false;
+
+        PuzzleManager.incrementPiecesPlaced();
+    }
+}
---
+++ SomniumStudios/Assets/_Scripts/Util/GameManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    public static GameManager instance = null;
+    [Tooltip("Screen fader controller")]
+    public GameObject fade = new GameObject();
+
+
+    void Awake() {
+        if (instance == null) {
+            instance = this;
+        }
+        else if (instance != this) {
+            Destroy(gameObject);
+        }
+
+        DontDestroyOnLoad(gameObject);
+
+        //TODO don't look at me T_T
+        fade = GameObject.Find("Fade");
+    }
+
+    public static void ChangeScene() {
+        fade.GetComponent("ScreenFader").fadeIn = false;
+        //TODO make this not hardcoded
+
+        SceneManager.LoadScene(0);
+
+    }
+
+    // Use this for initialization
+    void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
