15a8d863c2f2a7eb84d1db76fa9b1ff724c0189b
isabellechalhoub <isabelle.chalhoub@gmail.com>
2017-03-08 03:00:51 -0700

    3d puzzle works!! yay! its 3 am! rip me!
    3d puzzle works: dragging, falling, snapping, fading in 2d sprite.
    fading out 3d still doesnt work but it has to do with the
    material...........which i know nothing about so im calling it good for
    now.

--- Assets/_Scripts/Components/Puzzle/Draggable.cs
+++ Assets/_Scripts/Components/Puzzle/Draggable.cs
@@ -2,15 +2,28 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(BoxCollider2D))]
+[RequireComponent(typeof(MeshCollider))]
 
 public class Draggable : MonoBehaviour {
     private Vector3 screenPoint;
     //private Vector3 offset;
-    public bool isDraggable = true;
-    public bool hasInterruptor = false;
-    public GameObject receptacle;
-    public GameObject interruptor;
+    [SerializeField]
+    private bool isDraggable = true;
+
+    [SerializeField]
+    private bool hasInterruptor = false;
+
+    [SerializeField]
+    private GameObject receptacle;
+
+    [SerializeField]
+    private GameObject interruptor;
+
+    [SerializeField]
+    private float fadeTime;
+
+    private Color receptacleColor;
+    private Color itemColor;
 
     //TODO put in puzzle manager
     private int layerCount = 0;
@@ -23,7 +36,7 @@ public class Draggable : MonoBehaviour {
 
             screenPoint = Camera.main.WorldToScreenPoint(gameObject.transform.position);
 
-           // offset = gameObject.transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z));
+            //offset = gameObject.transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z));
             Cursor.visible = false;
         }
     }
@@ -41,7 +54,7 @@ public class Draggable : MonoBehaviour {
 
     void OnMouseUp()
     {
-        if (receptacle.GetComponent<BoxCollider2D>().bounds.Contains(Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 1000f))))
+        if (receptacle.GetComponent<BoxCollider2D>().bounds.Contains(new Vector3(gameObject.transform.position.x, gameObject.transform.position.y, 0f)));
         {
             if (hasInterruptor)
             {
@@ -56,11 +69,12 @@ public class Draggable : MonoBehaviour {
 
     void Snap()
     {
+        gameObject.GetComponent<Rigidbody>().isKinematic = true;
         Transform parentLocation = receptacle.transform;
 
         transform.position = parentLocation.position;
 
-        transform.rotation = new Quaternion(0f, 0f, 0f, 0f);
+        //transform.rotation = new Quaternion(0f, 0f, 0f, 0f);
 
       //  GetComponent<SpriteRenderer>().sortingOrder = -1;
 
@@ -69,9 +83,41 @@ public class Draggable : MonoBehaviour {
         if (hasInterruptor)
             interruptor.GetComponent<Interruptor>().setClickable(false);
 
-        GetComponent<BoxCollider2D>().enabled = false;
+        GetComponent<MeshCollider>().enabled = false;
         receptacle.GetComponent<BoxCollider2D>().enabled = false;
 
         PuzzleManager.incrementPiecesPlaced();
+        gameObject.GetComponent<MeshRenderer>().enabled = false;
+        //StartCoroutine(FadeOut());
+        StartCoroutine(FadeIn());
+    }
+
+    public IEnumerator FadeOut()
+    {
+        itemColor = gameObject.GetComponent<MeshRenderer>().material.color;
+        while (itemColor.a > 0)
+        {
+            itemColor.a -= 0.01f;
+            gameObject.GetComponent<MeshRenderer>().material.color = itemColor;
+            yield return new WaitForSeconds(fadeTime);
+        }
+        gameObject.SetActive(false);
+    }
+
+    public IEnumerator FadeIn()
+    {
+        receptacleColor = receptacle.GetComponent<SpriteRenderer>().color;
+        while (receptacleColor.a < 1)
+        {
+            receptacleColor.a += 0.01f;
+            receptacle.GetComponent<SpriteRenderer>().color = receptacleColor;
+            yield return new WaitForSeconds(fadeTime);
+        }
+        StartCoroutine(Wait(0.5f));
+    }
+    public IEnumerator Wait(float time)
+    {
+        yield return new WaitForSecondsRealtime(time);
+        gameObject.SetActive(false);
     }
 }
