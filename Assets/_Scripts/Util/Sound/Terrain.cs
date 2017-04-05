171289be2548b2083c5de4fc56ec56e6db5934b1
TROGDOR\dough <doughtnerd@gmail.com>
2017-04-05 01:35:14 -0600

    Example for terrain idea

---
+++ Assets/_Scripts/Util/Sound/Terrain.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Somnium
+{
+    public class Terrain : ITerrain
+    {
+        /// <summary>
+        /// Have all the possible sounds for the terrain loaded here.
+        /// </summary>
+        [SerializeField]
+        private List<AudioClip> sounds;
+
+        /// <summary>
+        /// Can either have a method per sound or...
+        /// </summary>
+        public void PlayFootstep()
+        {
+            SoundManager.instance.PlaySingle(sounds[0]);
+        }
+
+        /// <summary>
+        /// ... Have a single method that specifies the sound type, ie Footstep, running, walking, splashing, etc.
+        /// </summary>
+        /// <param name="soundType"></param>
+        public void PlaySound(TerrainSound soundType)
+        {
+            switch (soundType)
+            {
+                case TerrainSound.FOOTSTEP:
+                    SoundManager.instance.PlaySingle(sounds[0]);
+                    break;
+            }
+        }
+    }
+}
