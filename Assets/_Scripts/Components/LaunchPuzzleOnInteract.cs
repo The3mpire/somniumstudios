2859c941b8613d3a34126e3ea35260c38e0821cf
TROGDOR\dough <doughtnerd@gmail.com>
2017-04-05 03:31:29 -0600

    Set up walls and forest scene

---
+++ Assets/_Scripts/Components/LaunchPuzzleOnInteract.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Somnium
+{
+    [RequireComponent(typeof(LaunchPuzzle))]
+    public class LaunchPuzzleOnInteract : MonoBehaviour, IInteractable
+    {
+        public void Interact()
+        {
+            this.gameObject.GetComponent<LaunchPuzzle>().Interact(1, 1);
+        }
+
+    }
+}
---
+++ Assets/_Scripts/Components/TriggerDialogue.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Somnium
+{
+    public class TriggerDialogue : MonoBehaviour
+    {
+        [SerializeField]
+        private string dialogFilePath;
+
+        [SerializeField]
+        private Sprite profileSprite;
+
+        [SerializeField]
+        private bool Repeatable;
+
+        void OnTriggerEnter()
+        {
+            DialogManager.Instance.ProfileSprite = profileSprite;
+            DialogManager.Instance.StartDialog(dialogFilePath);
+            this.gameObject.SetActive(Repeatable);
+        }
+    }
+}
