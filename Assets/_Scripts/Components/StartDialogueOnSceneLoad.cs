a142aa72258e72151f0f1c04ae7f3c8b2b2258e3
TROGDOR\dough <doughtnerd@gmail.com>
2017-05-03 23:06:23 -0600

    Added StartDialogueOnSceneLoad in order to start Lucie dialogue depending on if the stoven puzzle was completed

---
+++ Assets/_Scripts/Components/StartDialogueOnSceneLoad.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Somnium
+{
+    public class StartDialogueOnSceneLoad : MonoBehaviour
+    {
+
+        [SerializeField]
+        private string dialogueFilePath;
+
+        [SerializeField]
+        private Sprite profileSprite;
+
+        [SerializeField]
+        private string flag;
+
+        [SerializeField]
+        private int flagValue;
+
+        private void OnLevelWasLoaded()
+        {
+            if (StateMachine.instance.GetFlag(flag)==flagValue)
+            {
+                DialogManager.Instance.ProfileSprite = profileSprite;
+                DialogManager.Instance.StartDialog(dialogueFilePath);
+            }
+        }
+    }
+}
